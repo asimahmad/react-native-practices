bfe740177f0a8669e6bedb171cf0f3efd2d9a9e3
Asim <asim005011997@gmail.com>
2021-06-30 12:11:32 +0530

    Added new way of fatching data using react-query

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import {
   SafeAreaView,
   ScrollView,
@@ -12,40 +13,70 @@ import {
 
 
 
-const App = () => {
+// const App = () => {
 
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState({});
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    async function getRepos() {
-      try {
-        const repoData = await fetch(
-          "https://api.github.com/repos/tannerlinsley/react-query"
-        ).then((res) => res.json());
-        setData(repoData);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    getRepos();
-  }, []);
-  // if (loading) return "Loading...";
+//   const [loading, setLoading] = useState(true);
+//   const [data, setData] = useState({});
+//   const [error, setError] = useState(null);
+//   useEffect(() => {
+//     async function getRepos() {
+//       try {
+//         const repoData = await fetch(
+//           "https://api.github.com/repos/tannerlinsley/react-query"
+//         ).then((res) => res.json());
+//         setData(repoData);
+//       } catch (error) {
+//         setError(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//     getRepos();
+//   }, []);
+//   // if (loading) return "Loading...";
 
-  // if (error) return "An error has occurred: " + error.message;
+//   // if (error) return "An error has occurred: " + error.message;
  
+//   return (
+//       <View>
+//         <Text style={{margin:'30%'}}>Hey there, I am Asim</Text>
+//         <Text>{data.name}</Text>
+//         <Text>{data.description}</Text>
+//         <Text>👀 {data.subscribers_count}</Text>
+//         <Text>✨ {data.stargazers_count}</Text>
+//         <Text>🍴 {data.forks_count}</Text>
+//       </View>
+//   );
+// };
+
+const queryClient = new QueryClient()
+
+function App() {
   return (
-      <View>
-        <Text style={{margin:'30%'}}>Hey there, I am Asim</Text>
-        <Text>{data.name}</Text>
-        <Text>{data.description}</Text>
-        <Text>👀 {data.subscribers_count}</Text>
-        <Text>✨ {data.stargazers_count}</Text>
-        <Text>🍴 {data.forks_count}</Text>
-      </View>
-  );
-};
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
+  )
+}
+
+function Example() {
+  const { isLoading, error, data } = useQuery('repoData', () =>
+    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res =>
+      res.json(),
+      console.log(res)
+    )
+  )
+
+  return (
+    <View>
+            <Text style={{margin:'30%'}}>Hey there, I am Asim</Text>
+            <Text>{data.name}</Text>
+            <Text>{data.description}</Text>
+            <Text>👀 {data.subscribers_count}</Text>
+            <Text>✨ {data.stargazers_count}</Text>
+            <Text>🍴 {data.forks_count}</Text>
+          </View>
+  )
+}
 
 export default App;
